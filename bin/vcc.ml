4140e32fc14a748c1bc7cfe267e2d4aa7fa8b6c6
Alberto Lazari <alberto.lazari@gmail.com>
2024-03-21 19:55:26 +0100

    Support string parsing

--- bin/vcc.ml
+++ bin/vcc.ml
@@ -2,6 +2,6 @@ open Vcc_lib
 
 let _ =
   try
-    Main.parse Sys.argv.(1)
+    Main.parse_file Sys.argv.(1)
   with
     Invalid_argument _ -> failwith "no input files"
--- lib/main.ml
+++ lib/main.ml
@@ -1,4 +1,8 @@
-let parse (file : string) =
+let parse str =
+  let lexbuf = Lexing.from_string str in
+  Parser.prog Lexer.read lexbuf
+
+let parse_file file =
   let c = open_in file in
   let lexbuf = Lexing.from_channel c in
   let p = Parser.prog Lexer.read lexbuf in
