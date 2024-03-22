a68b489519db0a53dc49f66313855228862e81a3
Alberto Lazari <alberto.lazari@gmail.com>
2024-03-22 12:14:41 +0100

    Fix

--- bin/vcc.ml
+++ bin/vcc.ml
@@ -1,7 +1,18 @@
 open Vcc_lib
 
-let _ =
-  try
-    Main.parse_file Sys.argv.(1)
-  with
-    Invalid_argument _ -> failwith "no input files"
+let parse_file file =
+  try Main.parse_file file
+  with _ -> Format.printf "[%s]\n*error*\n" file
+
+let rec parse_files files = match files with
+  | [] -> failwith "no input files"
+  | [file] -> parse_file file
+  | file :: files ->
+      parse_file file; print_endline "";
+      parse_files files
+
+let files = match Array.to_list Sys.argv with
+  | [] -> []
+  | _ :: fs -> fs
+
+let _ = parse_files files
--- lib/main.ml
+++ lib/main.ml
@@ -3,6 +3,6 @@ let parse_file file =
   let lexbuf = Lexing.from_channel c in
   let pi = Parser.prog Lexer.read lexbuf in
   close_in c;
-  Format.printf "[%s] %a@."
+  Format.printf "[%s]\n%a@."
     file
     Pretty_print.pp_prog pi
