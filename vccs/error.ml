d6a21be2fd31466e3a61287683c58e2649cfa5db
Alberto Lazari <alberto.lazari@gmail.com>
2024-03-23 22:58:05 +0100

    Make marker

--- vccs/error.ml
+++ vccs/error.ml
@@ -26,11 +26,37 @@ let column lexbuf pos =
   let newline_pos = newline_before_pos pos - 1 in
   pos - newline_pos
 
+let get_entire_line_at_position lexbuf pos =
+  let buffer_length = Bytes.length lexbuf.Lexing.lex_buffer in
+  if buffer_length = 0 then
+    "" (* Return an empty string if the buffer is empty *)
+  else
+    let rec find_line_start end_pos =
+      if end_pos = 0 || Bytes.get lexbuf.Lexing.lex_buffer (end_pos - 1) = '\n' then
+        end_pos
+      else
+        find_line_start (end_pos - 1)
+    in
+    let rec find_line_end start_pos =
+      if start_pos = buffer_length || Bytes.get lexbuf.Lexing.lex_buffer start_pos = '\n' then
+        start_pos
+      else
+        find_line_end (start_pos + 1)
+    in
+    let line_start_pos = find_line_start pos in
+    let line_end_pos = find_line_end pos in
+    let line_content = Bytes.sub_string lexbuf.Lexing.lex_buffer line_start_pos (line_end_pos - line_start_pos) in
+    line_content
+
 let report lexbuf =
   let curr = Lexing.lexeme lexbuf in
   let pos = lexbuf.Lexing.lex_curr_p.Lexing.pos_cnum in
   let ln = line lexbuf pos in
   let col = column lexbuf pos in
-  Format.sprintf "[!!] Syntax error at line %d col %d: unexpected token '%s'\n"
+  let line = get_entire_line_at_position lexbuf pos in
+  let marker = String.make (col - 1) ' ' ^ "^" in
+  Format.sprintf "%s\n%s\n[!!] Syntax error at line %d col %d: unexpected token '%s'\n"
+    line
+    marker
     ln col
     curr
