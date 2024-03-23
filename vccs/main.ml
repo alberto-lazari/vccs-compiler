b95b97a0eabc129350501662e8f32f0d089124b2
Alberto Lazari <alberto.lazari@gmail.com>
2024-03-23 22:22:04 +0100

    Find line position

--- vccs/main.ml
+++ vccs/main.ml
@@ -3,11 +3,7 @@ let parse lexbuf =
     Parser.prog Lexer.read lexbuf
   with
   | Parsing.Parse_error | Parser.Error | _ ->
-    let curr = Lexing.lexeme lexbuf in
-    let pos = Lexing.lexeme_start lexbuf in
-    let spaces = String.make pos ' ' in
-    Format.sprintf "%s^\n[!!] Syntax error at position %d: Unexpected token '%s'\n"
-      spaces pos curr |> failwith
+    Error.report lexbuf |> failwith
 
 let parse_file file =
   let c = open_in file in
