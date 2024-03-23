956ecfcffa3b2aedb5502be65600897b7315dbc9
Alberto Lazari <alberto.lazari@gmail.com>
2024-03-23 11:26:39 +0100

    Rename and restructure

--- bin/vcc.ml
+++ bin/vcc.ml
@@ -1,21 +1,23 @@
-open Vcc_lib
+open Vccs
 
-let parse_file file =
-  try Main.parse_file file
+let print_parse_file file =
+  try Format.printf "[%s]@.%a@.%!"
+    file
+    Pretty_print.pp_prog (Main.parse_file file)
   with
   | Sys_error err -> Printf.eprintf "[!!] %s\n" err
   | _ -> Format.printf "[%s]\n[!!] syntax error\n%!" file
 
-let rec parse_files files = match files with
+let rec print_parse_files files = match files with
   | [] -> failwith "no input files"
-  | [file] -> parse_file file
+  | [file] -> print_parse_file file
   | file :: files ->
-      parse_file file; print_endline "";
-      parse_files files
+      print_parse_file file; print_endline "";
+      print_parse_files files
 
 (* Discard $0 *)
 let files = match Array.to_list Sys.argv with
   | [] -> []
   | _ :: fs -> fs
 
-let () = parse_files files
+let () = print_parse_files files
--- vccs/main.ml
+++ vccs/main.ml
@@ -3,6 +3,4 @@ let parse_file file =
   let lexbuf = Lexing.from_channel c in
   let pi = Parser.prog Lexer.read lexbuf in
   close_in c;
-  Format.printf "[%s]@.%a@.%!"
-    file
-    Pretty_print.pp_prog pi
+  pi
