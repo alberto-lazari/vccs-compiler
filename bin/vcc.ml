d6ccf06e3f520301b297266c1c8f194cb86fd0e6
Alberto Lazari <alberto.lazari@gmail.com>
2024-03-26 00:01:52 +0100

    Working error management

--- bin/vcc.ml
+++ bin/vcc.ml
@@ -1,7 +1,7 @@
 open Vccs
 open Encoder
 
-let usage_msg = "usage: vcc [-i min..max] <input-file> [-o <output-file>]\noptions:"
+let usage_msg = "Usage: vcc [-i min..max] <input-file> [-o <output-file>]\nOptions:"
 let interval_string = ref "0..9"
 let input_file = ref ""
 let output_file = ref ""
@@ -49,38 +49,49 @@ let _print_encoded_file (interval : int * int) (file : string) =
   try print_iterated_file encode_file Ccs.Pretty_print.pp_prog file
   with Eval_error msg -> Printf.eprintf "[%s]\n%s%!" file msg
 
+let test_input_file file = match file with
+  | "" ->
+      Printf.eprintf "Error: no input files\n%s" (Arg.usage_string speclist usage_msg);
+      exit (1)
+  | _ -> try match Sys.is_directory file with
+      | true ->
+          Printf.eprintf "Error: '%s' is a directory\n" file;
+          exit (1)
+      | false -> ()
+    with Sys_error _ ->
+      Printf.eprintf "Error: file '%s' does not exist\n" file;
+      exit (1)
+
 let compile (interval : int * int) (output_file : string) (input_file : string) =
-  match input_file with
-    | "" -> Printf.eprintf "%s" (Arg.usage_string speclist usage_msg);
-        exit (1)
-    | input_file ->
-        let module Interval = struct let interval = interval end in
-        let open Encoder (Interval) in
-        let out = open_out output_file in
-        let fmt = Format.formatter_of_out_channel out in
-        try Format.fprintf fmt "%a@."
-          Ccs.Pretty_print.pp_prog (encode_file input_file);
-          close_out out
-        with
-        | Sys_error msg ->
-            Printf.eprintf "[!!] System error: %s\n" msg;
-            close_out out;
-            exit (1)
-        | Eval_error msg ->
-            Printf.eprintf "%s" msg;
-            close_out out;
-            exit (1)
-        | Failure msg ->
-            Printf.eprintf "%s" msg;
-            close_out out;
-            exit (1)
+  test_input_file input_file;
+  let module Interval = struct let interval = interval end in
+  let open Encoder (Interval) in
+  let out = open_out output_file in
+  let fmt = Format.formatter_of_out_channel out in
+  try Format.fprintf fmt "%a@."
+    Ccs.Pretty_print.pp_prog (encode_file input_file);
+    close_out out
+  with
+  | Eval_error msg ->
+      Printf.eprintf "%s" msg;
+      close_out out;
+      exit (1)
+  | Failure msg ->
+      Printf.eprintf "%s" msg;
+      close_out out;
+      exit (1)
 
 
 let () =
   Arg.parse speclist anon_fun usage_msg;
-  let output_file = if !output_file = ""
-    then Str.replace_first
-      (Str.regexp {|.*/\([^/]*\)\.vccs|}) {|\1.ccs|}
+  let output_file = if !output_file = "" then
+    [ (* Change extension to .ccs *)
+      ( (Str.regexp {|\([^.]*\)\.?[^.]*$|}), {|\1.ccs|} );
+      (* Keep basename *)
+      ( (Str.regexp {|\([^/]*/\)*\([^/]*\)|}), {|\2|} );
+    ] |> List.fold_left
+      (fun acc (pattern, replace) ->
+        Str.replace_first pattern replace acc)
       !input_file
     else !output_file
   in
