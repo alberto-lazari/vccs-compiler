9460edef0637379df6a66d57a5ade7005b67384e
Alberto Lazari <alberto.lazari@gmail.com>
2024-03-25 18:01:59 +0100

    Help message

--- bin/vcc.ml
+++ bin/vcc.ml
@@ -1,13 +1,29 @@
 open Vccs
 open Encoder
 
-module Interval = struct
-  let interval = (0, 5)
-end
-open Encoder (Interval)
+let usage_msg = "usage: vcc [-i min..max] <input-file> [-o <output-file>]\noptions:"
+let interval_string = ref "0..9"
+let input_file = ref ""
+let output_file = ref ""
+
+let anon_fun file =
+  input_file := file
+
+let rec speclist =[
+  ("-i", Arg.Set_string interval_string, "\t  values interval (ℕ set)");
+  ("-o", Arg.Set_string output_file, "\t  output file name (default=input-file.ccs)");
+  ("-help",
+    Unit (fun _ -> print_endline (Arg.usage_string speclist usage_msg); exit 0),
+    "  show this message");
+  ("--help",
+    Unit (fun _ -> print_endline (Arg.usage_string speclist usage_msg); exit 0),
+    " show this message");
+]
 
 let rec iterate_files (f : string -> unit) (files : string list) = match files with
-  | [] -> failwith "no input files"
+  | [] | [""] ->
+      Printf.eprintf "[!!] Error: no input files\n";
+      exit (1)
   | [file] -> f file
   | file :: files ->
       f file; print_endline "";
@@ -17,7 +33,7 @@ let print_iterated_file (f : string -> 'a) (pp : Format.formatter -> 'a -> unit)
   try Format.printf "[%s]@.%a@.%!" file
     pp (f file)
   with
-  | Sys_error err -> Printf.eprintf "[!!] %s\n%!" err
+  | Sys_error err -> Printf.eprintf "[!!] System error%s\n%!" err
   | Failure e -> Printf.eprintf "[%s]\n%s%!" file e
 
 
@@ -25,14 +41,15 @@ let print_iterated_file (f : string -> 'a) (pp : Format.formatter -> 'a -> unit)
 let _print_parsed_file (file : string) =
   print_iterated_file Main.parse_file Pretty_print.pp_prog file
 
-let print_encoded_file (file : string) =
+let print_encoded_file (interval : int * int) (file : string) =
+  let module Interval = struct let interval = interval end in
+  let open Encoder (Interval) in
+
   try print_iterated_file encode_file Ccs.Pretty_print.pp_prog file
   with Eval_error msg -> Printf.eprintf "[%s]\n%s%!" file msg
 
 
-(* Discard $0 *)
-let files = match Array.to_list Sys.argv with
-  | [] -> []
-  | _ :: fs -> fs
-
-let () = iterate_files print_encoded_file files
+let () =
+  Arg.parse speclist anon_fun usage_msg;
+  let interval = Scanf.sscanf !interval_string "%d..%d" (fun min max -> (min, max)) in
+  iterate_files (print_encoded_file interval) [!input_file]
