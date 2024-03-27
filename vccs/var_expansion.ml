1b160ff44e34620893fbfbe13c0033f32df7fda0
Alberto Lazari <alberto.lazari@gmail.com>
2024-03-27 22:14:11 +0100

    Document

--- vccs/var_expansion.ml
+++ vccs/var_expansion.ml
@@ -3,6 +3,30 @@ open Var_substitution
 
 let ch_n ch n = Printf.sprintf "%s_%d" ch n
 
+(*
+  (Unused) Expand input-bound variables for each value in domain:
+  let domain = {min..max}
+
+  a(x). P(x) -> a_min. P(min) + a_min+1. P(min+1)  + ... + a_max. P(max)
+*)
+let rec expand_act_var domain a p = match a with
+  | Input (ch, x) ->
+      begin match domain with
+      | [] -> Nil
+      | n :: [] -> Act (Input (ch_n ch n, x), substitute_proc_var x n p)
+      | n :: rest -> Sum (
+          Act (Input (ch_n ch n, x), substitute_proc_var x n p),
+          expand_act_var rest a p
+        )
+      end
+  | a -> Act (a, p)
+
+(*
+  Expand redirected channels for each value in domain:
+  let domain = {min..max}
+
+  P[a/b] -> P[a_min/b_min, a_min+1/b_min+1, ..., a_max/b_max]
+*)
 let rec expand_f_var domain f = match f with
   | [] -> []
   | (a, b) :: rest ->
@@ -11,6 +35,12 @@ let rec expand_f_var domain f = match f with
       in
       expanded_red @ (expand_f_var domain rest)
 
+(*
+  Expand restricted channels for each value in domain:
+  let domain = {min..max}
+
+  P \ a -> P \ {a_min, a_min+1, ..., a_max}
+*)
 let rec expand_resL_var domain l = match l with
   | [] -> []
   | a :: acts ->
@@ -19,6 +49,13 @@ let rec expand_resL_var domain l = match l with
       in
       expanded_ch @ (expand_resL_var domain acts)
 
+(*
+  Expand process definition parameters for each value in domain:
+  let domain = {min..max}
+
+  P(x) = proc(x); pi ->
+  P_min = proc(min); P_min+1 = proc(min+1); ...; P_max = proc(max); pi
+*)
 let rec expand_prog_var domain first pi =
   let sep = if first then "_" else "," in
   match pi with
